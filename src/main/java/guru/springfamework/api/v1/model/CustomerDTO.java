fafd88ee7b8a21339f9f74372350513d1e21cf73
rajesh23-drg <rmanjunath@teamdrg.com>
2018-11-05 07:53:10 +0530

    Customizing swagger docs

--- src/main/java/guru/springfamework/api/v1/model/CustomerDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/CustomerDTO.java
@@ -1,5 +1,6 @@
 package guru.springfamework.api.v1.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CustomerDTO {
+    @ApiModelProperty(value = "This is the first name", required = true)
     private String firstname;
+
+    @ApiModelProperty(required = true )
     private String lastname;
     // to change property in json returning object
     @JsonProperty("customer_url")
