887f9597b4eabbda58f642b991212eadb49eaed8
rajesh23-drg <rmanjunath@teamdrg.com>
2018-11-05 06:57:29 +0530

    Added Vendors

--- src/test/java/guru/springfamework/services/CustomerServiceImplIT.java
+++ src/test/java/guru/springfamework/services/CustomerServiceImplIT.java
@@ -6,6 +6,7 @@ import guru.springfamework.bootstrap.Bootstrap;
 import guru.springfamework.domain.Customer;
 import guru.springfamework.repositories.CategoryRepository;
 import guru.springfamework.repositories.CustomerRepository;
+import guru.springfamework.repositories.VendorRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,13 +27,16 @@ public class CustomerServiceImplIT {
     CustomerRepository customerRepository;
     @Autowired
     CategoryRepository categoryRepository;
+    @Autowired
+    VendorRepository vendorRepository;
+
     CustomerService customerService;
     @Before
     public void setUp() throws Exception {
         System.out.println("Loading Customer Data");
         System.out.println(customerRepository.findAll().size());
         //setup data for testing
-        Bootstrap bootstrap = new Bootstrap(categoryRepository, customerRepository);
+        Bootstrap bootstrap = new Bootstrap(categoryRepository, customerRepository, vendorRepository);
         bootstrap.run(); //load data
         customerService = new CustomerServiceImpl(CustomerMapper.INSTANCE, customerRepository);
     }
